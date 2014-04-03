b7857aaaa218a540ec925ca432122c119848f97f
Shuhao Wu <shuhao@shuhaowu.com>
2014-04-03 13:59:16 -0400

    Problem with kap10 deployment and restarts

--- configs/kap10-deploy.sh
+++ configs/kap10-deploy.sh
@@ -3,3 +3,4 @@
 echo "Trying to deploy $1..."
 cd /home/kap10/app
 source /home/kap10/venv/bin/activate && fab deploy:commit=$1,run=True --host localhost --user root -i /home/kap10/root_local_key
+echo "To restart kap10, you need to manually go into the server and do monit restart."
